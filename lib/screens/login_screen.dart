7122a7d955a1c67f8b3bb5b9d5c916334e9ac250
Malik Abdullah <malikabdullah130037@gmail.com>
2022-12-24 13:40:38 +0500

    creating loginscreen  file

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Start or join a meeting",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image.asset('assets/onbording.jpg'),
+          ],
+        ),
+      ),
+    );
+  }
+}
